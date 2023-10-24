2a865d22e40c5812e387eda82a8a32bd943c5b5b
huzaifak08 <hk7928042@gmail.com>
2023-10-24 11:00:18 +0500

    Introduction

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello World We are learning Golang Allhamdullilah")
+
+	fmt.Println(time.Now())
+
+	fmt.Println("My favourite number is ", rand.Intn(10))
+
+	fmt.Println("The square root of 7 is ", math.Sqrt(7))
+}
